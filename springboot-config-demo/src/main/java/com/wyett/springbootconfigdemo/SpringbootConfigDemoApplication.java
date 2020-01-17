1a3b86fc5d31302ee9a478ba49011da8393736ee
D1-92 <wyettlei@hotmail.com>
2020-01-17 19:10:52 +0800

    read application.properties

---
+++ springboot-config-demo/src/main/java/com/wyett/springbootconfigdemo/SpringbootConfigDemoApplication.java
@@ -0,0 +1,13 @@
+package com.wyett.springbootconfigdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootConfigDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootConfigDemoApplication.class, args);
+    }
+
+}
