efc95ca300812a3a2a5a11ac629b0305a9516556
D1-92 <wyettlei@hotmail.com>
2020-03-13 17:11:12 +0800

    spring-boot redis demo

---
+++ springboot-redis-demo/src/main/java/com/wyett/SpringBootRedisApplication.java
@@ -0,0 +1,17 @@
+package com.wyett;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : wyettLei
+ * @date : Created in 2020/3/13 9:14
+ * @description: TODO
+ */
+
+@SpringBootApplication
+public class SpringBootRedisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRedisApplication.class, args);
+    }
+}
