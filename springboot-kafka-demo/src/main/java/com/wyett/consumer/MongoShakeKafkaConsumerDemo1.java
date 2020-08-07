adc49b7c6904d182b212be9a357e13881f34a050
D1-92 <wyettlei@hotmail.com>
2020-08-07 18:54:26 +0800

    spring boot kafka

---
+++ springboot-kafka-demo/src/main/java/com/wyett/consumer/MongoShakeKafkaConsumerDemo1.java
@@ -0,0 +1,24 @@
+package com.wyett.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : wyettLei
+ * @date : Created in 2020/7/28 11:32
+ * @description: TODO
+ */
+
+@Component
+public class MongoShakeKafkaConsumerDemo1 {
+
+
+    @KafkaListener(topics = "mongoshake_test1_topic", groupId = "mygroup1")
+    public void listen(ConsumerRecord<String, String> record) {
+        System.out.println(record.topic() + " - " + record.key() + ":" + record.value());
+
+    }
+}
---
+++ springboot-kafka-demo/src/main/java/com/wyett/consumer/MongoShakeKafkaConsumerMain.java
@@ -0,0 +1,17 @@
+package com.wyett.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : wyettLei
+ * @date : Created in 2020/7/28 11:36
+ * @description: TODO
+ */
+
+@SpringBootApplication
+public class MongoShakeKafkaConsumerMain {
+    public static void main(String[] args) {
+        SpringApplication.run(MongoShakeKafkaConsumerMain.class, args);
+    }
+}
