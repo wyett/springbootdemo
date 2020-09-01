ad15127b9956e1c42c353603a6f26aea266d0fae
D1-92 <wyettlei@hotmail.com>
2020-09-01 18:36:54 +0800

    spring boot jsp

---
+++ springboot-web-jsp-demo/src/main/java/com/wyett/JSPApplication.java
@@ -0,0 +1,17 @@
+package com.wyett;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : wyettLei
+ * @date : Created in 2020/9/1 17:13
+ * @description: TODO
+ */
+
+@SpringBootApplication
+public class JSPApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JSPApplication.class, args);
+    }
+}
---
+++ springboot-web-jsp-demo/src/main/java/com/wyett/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.wyett.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author : wyettLei
+ * @date : Created in 2020/9/1 17:11
+ * @description: TODO
+ */
+
+@Controller
+@RequestMapping("/cat")
+public class TestController {
+
+    @RequestMapping("")
+    public ModelAndView index() {
+        ModelAndView mv = new ModelAndView("/index");
+        mv.addObject("hi", "hello, cat");
+        return mv;
+    }
+}
