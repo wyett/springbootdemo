db080591e7f703b4fcbf20d5f31da71208036f76
D1-92 <wyettlei@hotmail.com>
2020-09-04 15:00:54 +0800

    spring boot mybatis-generator demo

---
+++ springboot-mybatis-generator-demo/src/main/java/com/wyett/springbootmybatisgeneratordemo/SpringbootMybatisGeneratorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.wyett.springbootmybatisgeneratordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisGeneratorDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMybatisGeneratorDemoApplication.class, args);
+    }
+
+}
---
+++ springboot-mybatis-generator-demo/src/test/java/com/wyett/springbootmybatisgeneratordemo/SpringbootMybatisGeneratorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyett.springbootmybatisgeneratordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMybatisGeneratorDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
