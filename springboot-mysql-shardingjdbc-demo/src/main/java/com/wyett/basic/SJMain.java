a06b093cf448f2c528f994792093169ef1b1daf3
wyettlei <wyettlei@hotmail.com>
2021-01-08 19:04:41 +0800

    spring

---
+++ springboot-mysql-shardingjdbc-demo/src/main/java/com/wyett/basic/SJMain.java
@@ -0,0 +1,17 @@
+package com.wyett.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : wyettLei
+ * @date : Created in 2021/1/8 19:03
+ * @description: TODO
+ */
+
+@SpringBootApplication
+public class SJMain {
+    public static void main(String[] args) {
+        SpringApplication.run(SJMain.class, args);
+    }
+}
